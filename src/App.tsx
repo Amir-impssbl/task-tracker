e2e6aa4e05880fa19b5cb549bc3aafc9f374133f
AmirHossein Motameni <a.h.motameni@gmail.com>
2024-12-19 16:55:38 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { AuthForm } from '@/components/auth/AuthForm';
+import { Toaster } from '@/components/ui/toaster';
+
+function App() {
+  return (
+    <>
+      <AuthForm />
+      <Toaster />
+    </>
+  );
+}
+
+export default App;
